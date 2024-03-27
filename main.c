bccab208a94c0cd83aab20930f849d64b76bdae1
Fatih BOZLAK <bozlak.fatih@gmail.com>
2024-03-27 11:50:28 +0100

    calcule de l'angle de la balle par rapport au ship

--- main.c
+++ main.c
@@ -13,12 +13,12 @@ Obj *createObj(double x, double y, double vx, double vy, double w, double h) {
 }
 
 int main() {
-    GameContext *gameContext = init(1000, 1000);
+    GameContext *gameContext = init(500, 500);
 
-    Obj *ball = createObj(0, 0, 0.0, -0.05, 10, 10);
+    Obj *ball = createObj(250, 250, 0.0, 0.0, 50, 50);
     gameContext->ball = ball;
 
-    Obj *ship = createObj(0, -0.9, 0.01, 0, 100, 10);
+    Obj *ship = createObj(250, 450, 10, 0, 100, 25);
     gameContext->ship = ship;
 
     gameContext->isGameRunning = true;
--- update.h
+++ update.h
@@ -5,15 +5,20 @@
 #ifndef ARKANOID_UPDATE_H
 #define ARKANOID_UPDATE_H
 
+#include <math.h>
 #include "type.h"
 
+void screenToWorld(GameContext *gameContext, Obj *obj, int screenX, int screenY) {
+    obj->x = screenX;
+    obj->y = screenY;
+}
+
 void worldToScreen(GameContext *gameContext, Obj *obj) {
-    obj->wireframe.x = (int) round(
-            (obj->x * (gameContext->window->w / 2.0)) + (gameContext->window->w / 2.0) - (obj->w / 2));
-    obj->wireframe.y = (int) round(
-            (-obj->y * (gameContext->window->h / 2.0)) + (gameContext->window->h / 2.0) - (obj->h / 2));
+    obj->wireframe.x = (int) round(obj->x - obj->w / 2);
+    obj->wireframe.y = (int) round(obj->y - obj->h / 2);
     obj->wireframe.w = (int) round(obj->w);
     obj->wireframe.h = (int) round(obj->h);
+
 }
 
 void update(GameContext *gameContext) {
@@ -23,6 +28,11 @@ void update(GameContext *gameContext) {
     while (SDL_PollEvent(&event)) {
         if (event.type == SDL_QUIT)
             gameContext->isGameRunning = false;
+        if (event.type == SDL_MOUSEMOTION) {
+            screenToWorld(gameContext, gameContext->ball, event.motion.x, event.motion.y);
+        }
+        if (event.type == SDL_MOUSEBUTTONDOWN) {
+        }
     }
 
     const Uint8 *keys = SDL_GetKeyboardState(NULL);
@@ -32,16 +42,19 @@ void update(GameContext *gameContext) {
     if (keys[SDL_SCANCODE_LEFT]) {
         gameContext->ship->x -= gameContext->ship->vx;
     }
+    if (keys[SDL_SCANCODE_SPACE]) {
+        printf("angle = %lf\n",
+               atan2(gameContext->ship->y - gameContext->ball->y,
+                     gameContext->ship->x - gameContext->ball->x
+               ) * 180 / M_PI
+        );
+    }
 
     Obj *ball = gameContext->ball;
     ball->x += ball->vx * gameContext->deltaTime;
     ball->y += ball->vy * gameContext->deltaTime;
 
     Obj *ship = gameContext->ship;
-    if (ship->x <= -1.0)
-        ship->x = -1.0;
-    else if (ship->x >= 1)
-        ship->x = 1.0;
 
     worldToScreen(gameContext, ball);
     worldToScreen(gameContext, ship);
